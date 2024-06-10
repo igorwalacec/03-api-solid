2ed602927205942ab554824fd6e0a34804169894
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2024-06-10 00:26:41 -0300

    refactor: organize paths

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
+import { appRoutes, usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { fastifyJwt } from '@fastify/jwt'
@@ -10,7 +10,9 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+// app.register(gymsRoutes)
+
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
     return reply
