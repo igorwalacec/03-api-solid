dbd45c5c561a43d49695acf99696e4e19cd4050a
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2024-06-10 01:53:38 -0300

    test: implement tests e2e refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,13 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 import { verifyJwt } from '@/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
