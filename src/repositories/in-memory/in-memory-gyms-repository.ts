418ed4e11d97bac6eef44100bae123f13e17b42f
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2024-06-09 21:26:56 -0300

    feat: create gym use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,25 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'crypto'
+import { Decimal } from '@prisma/client/runtime/library'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym: Gym = {
+      id: randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+    }
+
+    this.items.push(gym)
+
+    return gym
+  }
 
   async findById(id: string) {
     const gym = this.items.find((gym) => gym.id === id)
---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to register', async () => {
+    const { gym } = await sut.execute({
+      title: 'Gym',
+      description: 'Gym Description',
+      latitude: 0,
+      longitude: 0,
+      phone: '123456',
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
